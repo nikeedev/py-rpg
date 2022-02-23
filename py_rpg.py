dbde9a09718193f0556fea1fb3feec6d0a4a58e7
nikeedev <myftr12394@gmail.com>
2022-02-23 16:22:33 +0100

    Updates

--- py_rpg.py
+++ py_rpg.py
@@ -1,4 +1,29 @@
 print("Starting game up...")
+
 import time
-import pygame
+
+
+
+
+game = [
+    ["", "", ""],
+    ["", "+", ""],
+    ["", "", ""]
+]
+
+
+def init():
+    for i in range(len(game)):
+        print(game[i])
+
+
+find_in_list_of_list(game, "+")
+
+# import pygame
 time.sleep(2)
+
+# while True:
+init()
+#    direct = str(input("What direction( Up(u), down(d), left(l) or right(r))? "))
+#    if direct == "u":
+#       up()
